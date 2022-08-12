cd6ecca6570011048500898a4655785f60395a09
csehamidur <hamidurrahman262@gmail.com>
2022-08-12 10:27:51 +0530

    Initial commit

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Asteroid : MonoBehaviour
+{
+    
+ 
+    public GameObject player;
+    Player ps;
+    int numberOfAsteriod;
+    [SerializeField]
+    float tumble;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+        player = GameObject.Find("Player");
+        ps = player.GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        var user = GameObject.FindGameObjectsWithTag("player");
+        Vector3 userPosition = player.transform.position;
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, userPosition, 3 * Time.deltaTime);
+    }
+
+
+    public void asteroidDestroy()
+    {
+     
+        GetComponent<AudioSource>().Play();
+        numberOfAsteriod = GameObject.FindGameObjectsWithTag("asteroid").Length;
+        if(numberOfAsteriod<=10)
+        {
+            GameObject asteroid = Instantiate(Resources.Load("Asteroid1")) as GameObject;
+            float x = Random.Range(-35f, 35f);
+            float z = Random.Range(45f, 100f);
+            float y = Random.Range(10f, 20f);
+            asteroid.transform.position = new Vector3(x, y, z);
+        }
+        
+        ps.shoot();
+        Destroy(gameObject, .5f);
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    CharacterController characterController;
+    public Transform cameraTransform;
+    GameObject gun;
+    public TextMeshProUGUI textMeshProUGUI;
+    public TextMeshProUGUI textMeshProUGUI2;
+    int finalScore;
+    bool gameOver = false;
+    public int score = 0;
+    public int life;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        characterController = gameObject.GetComponent<CharacterController>();
+        gun = gameObject.transform.GetChild(0).gameObject.transform.GetChild(1).gameObject;
+        score = 0;
+        life = 3;
+        textMeshProUGUI.text = "Score: " + score;
+        finalScore = score;
+    }
+
+    public void shoot()
+    {
+        StartCoroutine("Shoot");
+    }
+    IEnumerator Shoot()
+    {
+       // gun.GetComponent<Animation>().Play();
+        yield return new WaitForSeconds(1f);
+        score += 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+}
